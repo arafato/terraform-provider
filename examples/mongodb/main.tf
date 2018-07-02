de5aa79707df2807b8d4a08f33afc3e45ccb1446
arafato <oliver.arafat@outlook.com>
2018-07-02 09:28:48 +0200

    added mongodb resource provider

--- examples/mongodb/main.tf
+++ examples/mongodb/main.tf
@@ -1,3 +1,10 @@
 data "alicloud_mongodb_instances" "mongo" {
   output_file = "out.dat"
 }
+
+resource "alicloud_mongodb_instance" "mymongo" {
+  instance_class   = "dds.mongo.mid"
+  instance_storage = "10"
+  engine_version   = "3.4"
+  description      = "foobar"
+}
