53e87ceca700bb95e319a697c6f278f4eb93afd0
arafato <oliver.arafat@outlook.com>
2018-07-03 13:49:23 +0200

    updated

--- examples/mongodb/main.tf
+++ examples/mongodb/main.tf
@@ -4,7 +4,8 @@ data "alicloud_mongodb_instances" "mongo" {
 
 resource "alicloud_mongodb_instance" "mymongo" {
   instance_class   = "dds.mongo.mid"
-  instance_storage = "10"
+  instance_storage = "20"
   engine_version   = "3.4"
   description      = "foobar"
+  security_ips     = ["127.0.0.1", "2.2.2.2"]
 }
