461ecf2ec4464353ca01ce58e09f9eec1723660d
arafato <oliver.arafat@outlook.com>
2018-10-09 13:26:00 +0200

    intermediate

--- alicloud/resource_alicloud_nas_filesystem.go
+++ alicloud/resource_alicloud_nas_filesystem.go
@@ -2,7 +2,6 @@ package alicloud
 
 import (
 	"fmt"
-	"strings"
 	"time"
 
 	"github.com/aliyun/alibaba-cloud-sdk-go/services/nas"
@@ -60,8 +59,6 @@ func resourceAlicloudNASFilesystemCreate(d *schema.ResourceData, meta interface{
 	client := meta.(*AliyunClient)
 	conn := client.nasconn
 
-	conn.DescribeFileSystems()
-
 	request := nas.CreateCreateFileSystemRequest()
 	request.StorageType = d.Get("storage_type").(string)
 	request.ProtocolType = d.Get("protocol_type").(string)
@@ -80,81 +77,14 @@ func resourceAlicloudNASFilesystemCreate(d *schema.ResourceData, meta interface{
 
 func resourceAlicloudNASFilesystemUpdate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*AliyunClient)
-	conn := client.rkvconn
+	conn := client.nasconn
 	d.Partial(true)
 
-	if d.HasChange("security_ips") {
-		request := r_kvstore.CreateModifySecurityIpsRequest()
-		request.SecurityIpGroupName = "default"
-		request.InstanceId = d.Id()
-		if len(d.Get("security_ips").(*schema.Set).List()) > 0 {
-			request.SecurityIps = strings.Join(expandStringList(d.Get("security_ips").(*schema.Set).List())[:], COMMA_SEPARATED)
-		} else {
-			return fmt.Errorf("Security ips cannot be empty")
-		}
-		// wait instance status is Normal before modifying
-		if err := client.WaitForRKVInstance(d.Id(), Normal, DefaultLongTimeout); err != nil {
-			return fmt.Errorf("WaitForInstance %s got error: %#v", Running, err)
-		}
-		if _, err := conn.ModifySecurityIps(request); err != nil {
-			return fmt.Errorf("Create security whitelist ips got an error: %#v", err)
-		}
-		d.SetPartial("security_ips")
-		// wait instance status is Normal after modifying
-		if err := client.WaitForRKVInstance(d.Id(), Normal, DefaultLongTimeout); err != nil {
-			return fmt.Errorf("WaitForInstance %s got error: %#v", Running, err)
-		}
-	}
-
-	if d.IsNewResource() {
-		d.Partial(false)
-		return resourceAlicloudNASFilesystemRead(d, meta)
-	}
-
-	if d.HasChange("instance_class") {
-		request := r_kvstore.CreateModifyInstanceSpecRequest()
-		request.InstanceId = d.Id()
-		request.InstanceClass = d.Get("instance_class").(string)
-		request.EffectiveTime = "Immediately"
-		if _, err := conn.ModifyInstanceSpec(request); err != nil {
-			return err
-		}
-		// wait instance status is Normal after modifying
-		if err := client.WaitForRKVInstance(d.Id(), Normal, DefaultLongTimeout); err != nil {
-			return fmt.Errorf("WaitForInstance %s got error: %#v", Running, err)
-		}
-
-		d.SetPartial("instance_class")
-	}
-
-	request := r_kvstore.CreateModifyInstanceAttributeRequest()
-	request.InstanceId = d.Id()
-	update := false
-	if d.HasChange("instance_name") {
-		request.InstanceName = d.Get("instance_name").(string)
-		update = true
-
-		d.SetPartial("instance_name")
-	}
-
-	if d.HasChange("password") {
-		request.NewPassword = d.Get("password").(string)
-		update = true
-		d.SetPartial("password")
-	}
-
-	if update {
-		// wait instance status is Normal before modifying
-		if err := client.WaitForRKVInstance(d.Id(), Normal, DefaultLongTimeout); err != nil {
-			return fmt.Errorf("WaitForInstance %s got error: %#v", Running, err)
-		}
-		if _, err := conn.ModifyInstanceAttribute(request); err != nil {
-			return fmt.Errorf("ModifyRKVInstanceDescription got an error: %#v", err)
-		}
-		// wait instance status is Normal after modifying
-		if err := client.WaitForRKVInstance(d.Id(), Normal, DefaultLongTimeout); err != nil {
-			return fmt.Errorf("WaitForInstance %s got error: %#v", Running, err)
-		}
+	if d.HasChange("description") {
+		request := nas.CreateModifyFileSystemRequest()
+		request.Description = d.Get("description").(string)
+		conn.ModifyFileSystem(request)
+		d.SetPartial("description")
 	}
 
 	d.Partial(false)
@@ -187,18 +117,15 @@ func resourceAlicloudNASFilesystemRead(d *schema.ResourceData, meta interface{})
 func resourceAlicloudNASFilesystemDelete(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*AliyunClient)
 
-	instance, err := client.DescribeRKVInstanceById(d.Id())
+	fs, err := client.DescribeNASFilesystemById(d.Id())
 	if err != nil {
 		if NotFoundError(err) {
 			return nil
 		}
-		return fmt.Errorf("Error Describe KVStore InstanceAttribute: %#v", err)
+		return fmt.Errorf("Error DescribeNASFilesystemById : %#v", err)
 	}
-	if PayType(instance.ChargeType) == Prepaid {
-		return fmt.Errorf("At present, 'Prepaid' instance cannot be deleted and must wait it to be expired and release it automatically")
-	}
-	request := r_kvstore.CreateDeleteInstanceRequest()
-	request.InstanceId = d.Id()
+
+	// Check if filesystem has mount points attached
 
 	return resource.Retry(5*time.Minute, func() *resource.RetryError {
 		_, err := client.rkvconn.DeleteInstance(request)
